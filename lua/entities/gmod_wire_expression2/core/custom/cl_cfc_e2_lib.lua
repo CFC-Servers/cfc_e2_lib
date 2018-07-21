2fb3acd16c0dafdd1955278fecd31cb67b67e39e
Justin Muskopf <birmesepython@gmail.com>
2018-07-21 00:58:08 -0500

    Recommended changes from PR #5

--- lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
@@ -1,7 +1,7 @@
 -- PvP Functions
 E2Helper.Descriptions["playerIsInPvp"]     = "Returns 1 if a player is in Pvp, and 0 otherwise."
 E2Helper.Descriptions["playerIsInBuild"]   = "Returns 1 if a player is in build, and 0 otherwise."
-E2Helper.Descriptions["getPvpStatus"]      = "Returns \"pvp\" if a player is in Pvp, and \"build\" if they are in build."
+E2Helper.Descriptions["getPvpStatus"]      = "Returns \"pvp\" if a player is in Pvp, and \"build\" if they are in build. If a player is invalid, or their status is unknown, an empty string is returned."
 E2Helper.Descriptions["getPvpers"]         = "Returns a table of all players currently in Pvp."
 E2Helper.Descriptions["getBuilders"]       = "Returns a table of all players currently in Build."
 
@@ -9,12 +9,12 @@ E2Helper.Descriptions["getBuilders"]       = "Returns a table of all players cur
 E2Helper.Descriptions["averagePing"] = "Returns a floating-point number representing the average ping of players in the server."
 
 -- String Functions
-E2Helper.Descriptions["startsWith"] = "Returns a 1 if the string on which it was called starts with start, and 0 otherwise."
+E2Helper.Descriptions["startsWith"] = "Returns 1 if the string on which it was called starts with start, and 0 otherwise."
 
 -- Array Functions
-E2Helper.Descriptions["shuffled"]   = "Returns a randomly shuffled version of an array."
-E2Helper.Descriptions["difference"] = "Returns an array filled with the differing elements between the calling and argument arrays."
-E2Helper.Descriptions["reverse"]    = "Returns a reversed array."
-E2Helper.Descriptions["sub"]        = "Returns an array representing the subset between indexes n1 and n2."
-E2Helper.Descriptions["sub"]        = "Returns an array representing the subset between index n1 and the end of the array."
-E2Helper.Descriptions["indexOf"]    = "Returns the FIRST index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["shuffled"]    = "Returns a shuffled array."
+E2Helper.Descriptions["difference"]  = "Returns an array filled with the differing elements between the calling and argument arrays."
+E2Helper.Descriptions["reversed"]    = "Returns a reversed array."
+E2Helper.Descriptions["sub"]         = "Returns an array representing the subset between indexes n1 and n2."
+E2Helper.Descriptions["sub"]         = "Returns an array representing the subset between index n1 and the end of the array."
+E2Helper.Descriptions["indexOf"]     = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
