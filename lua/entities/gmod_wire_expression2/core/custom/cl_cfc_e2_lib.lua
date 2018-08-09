1e0ed731058e85b8853fbf5a98576a6284676979
Justin Muskopf <birmesepython@gmail.com>
2018-08-09 18:56:25 -0500

    Add documentation

--- lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
@@ -12,15 +12,16 @@ E2Helper.Descriptions["averagePing()"] = "Returns a floating-point number repres
 E2Helper.Descriptions["startsWith(s:s)"] = "Returns 1 if the string on which it was called starts with start, and 0 otherwise."
 
 -- Array Functions
-E2Helper.Descriptions["shuffled(r:)"]    = "Returns a shuffled array."
-E2Helper.Descriptions["difference(r:r)"] = "Returns an array filled with the differing elements between the calling and argument arrays."
-E2Helper.Descriptions["reversed(r:)"]    = "Returns a reversed array."
-E2Helper.Descriptions["sub(r:nn)"]       = "Returns an array representing the subset between indexes n1 and n2, inclusively."
-E2Helper.Descriptions["sub(r:n)"]        = "Returns an array representing the subset between index n and the end of the array, inclusively."
-E2Helper.Descriptions["indexOf(r:e)"]    = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
-E2Helper.Descriptions["indexOf(r:n)"]    = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
-E2Helper.Descriptions["indexOf(r:s)"]    = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
-E2Helper.Descriptions["indexOf(r:a)"]    = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
-E2Helper.Descriptions["indexOf(r:v)"]    = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["shuffled(r:)"]      = "Returns a shuffled array."
+E2Helper.Descriptions["difference(r:r)"]   = "Returns an array filled with the differing elements between the calling and argument arrays."
+E2Helper.Descriptions["intersection(r:r)"] = "Rturns an array filled with the similar elements between the calling and argument arrays."
+E2Helper.Descriptions["reversed(r:)"]      = "Returns a reversed array."
+E2Helper.Descriptions["sub(r:nn)"]         = "Returns an array representing the subset between indexes n1 and n2, inclusively."
+E2Helper.Descriptions["sub(r:n)"]          = "Returns an array representing the subset between index n and the end of the array, inclusively."
+E2Helper.Descriptions["indexOf(r:e)"]      = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["indexOf(r:n)"]      = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["indexOf(r:s)"]      = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["indexOf(r:a)"]      = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
+E2Helper.Descriptions["indexOf(r:v)"]      = "Returns the first index at which the value is stored in the array, and 0 if it was not found."
 
 
