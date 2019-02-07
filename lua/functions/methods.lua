18d5854c15df77d5cf0a10934bb4cf794b273282
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-02-07 13:13:00 -0800

    Add more ping and loss functions

--- lua/functions/methods.lua
+++ lua/functions/methods.lua
@@ -1,7 +1,9 @@
 e2function array getPvpers()
     local pvpers = {}
-    for _, v in pairs(player.GetHumans()) do
-        if v:GetNWBool( "CFC_PvP_Mode", false ) then pvpers[#pvpers + 1] = v end
+    local humans = player.GetHumans()
+
+    for _, human in pairs( humans ) do
+        if human:GetNWBool( "CFC_PvP_Mode", false ) then pvpers[#pvpers + 1] = human end
     end
 
     return pvpers
@@ -9,8 +11,10 @@ end
 
 e2function array getBuilders()
     local builders = {}
-    for _, v in pairs(player.GetHumans()) do
-        if v:GetNWBool( "CFC_PvP_Mode", false ) == false then builders[#builders + 1] = v end
+    local humans = player.GetHumans()
+
+    for _, human in pairs( humans ) do
+        if human:GetNWBool( "CFC_PvP_Mode", false ) == false then builders[#builders + 1] = human end
     end
 
     return builders
@@ -21,11 +25,79 @@ e2function number averagePing()
     local sum = 0
     local humans = player.GetHumans()
 
-    for _, v in pairs( humans ) do
-        sum = sum + v:Ping()
+    for _, human in pairs( humans ) do
+        sum = sum + human:Ping()
     end
 
-    local avg = sum / table.Count( humans )
+    local average = sum / table.Count( humans )
 
-    return avg
+    return average
+end
+
+e2function number minPing()
+    local min = 0
+    local humans = player.GetHumans()
+
+    for _, human in pairs( humans ) do
+        local ping = human:Ping()
+        if ping < min then min = ping end
+    end
+
+    return min
+end
+
+e2function number maxPing()
+    local max = 0
+    local humans = player.GetHumans()
+
+    for i, human in pairs( humans ) do
+        local ping = human:Ping()
+
+        if i == 1 then max = ping end
+
+        if ping > max then max = ping end
+    end
+
+    return max
+end
+
+-- Loss Functions
+e2function number averageLoss()
+    local sum = 0
+    local humans = player.GetHumans()
+
+    for _, human in pairs( humans ) do
+        sum = sum + human:PacketLoss()
+    end
+
+    local average = sum / table.Count( humans )
+
+    return average
+end
+
+e2function number minLoss()
+    local min = 0
+    local humans = player.GetHumans()
+
+    for _, human in pairs( humans ) do
+        local loss = human:PacketLoss()
+        if loss < min then min = loss end
+    end
+
+    return min
+end
+
+e2function number maxLoss()
+    local max = 0
+    local humans = player.GetHumans()
+
+    for i, human in pairs( humans ) do
+        local loss = human:PacketLoss()
+
+        if i == 1 then max = loss end
+
+        if loss > max then max = loss end
+    end
+
+    return max
 end
