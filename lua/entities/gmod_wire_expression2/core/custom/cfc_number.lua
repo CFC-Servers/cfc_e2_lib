7bfba01aeef3eb8a9a0153e42f63d14ec1d7133c
Logica <16617746+TechnichalDoge@users.noreply.github.com>
2019-05-04 12:41:51 -0500

    Shorten this

--- lua/entities/gmod_wire_expression2/core/custom/cfc_number.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_number.lua
@@ -3,9 +3,8 @@ E2Lib.RegisterExtension( "cfc_e2_lib", true )
 __e2setcost(3)
 
 e2function vector2 number:plusMinus(number otherNum)
-    local firstNum = this
     return {
-            firstNum + otherNum,
-            firstNum - otherNum
+            this + otherNum,
+            this - otherNum
     }
 end
