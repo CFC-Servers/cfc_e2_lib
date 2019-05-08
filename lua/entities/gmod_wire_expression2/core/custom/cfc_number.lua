d88c6216162fd4fae8bbb2e64473a3a13db59043
= <damierny@gmail.com>
2019-05-08 18:16:22 -0500

    Removed everything custom but the booleans

--- lua/entities/gmod_wire_expression2/core/custom/cfc_number.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_number.lua
@@ -1,17 +1,1 @@
 E2Lib.RegisterExtension( "cfc_e2_lib", true )
-
-__e2setcost(1)
-
-e2function vector2 number:plusMinus(number otherNum)
-    return {
-            this + otherNum,
-            this - otherNum
-    }
-end
-
-e2function vector2 plusMinus(number firstNum, number otherNum)
-    return {
-            firstNum + otherNum,
-            firstNum - otherNum
-    }
-end
