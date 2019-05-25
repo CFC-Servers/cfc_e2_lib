c104146ecb0078f491264e30be91dd94c5cb9cbb
PLally <11771225+PLally@users.noreply.github.com>
2019-05-24 22:49:42 -0400

    create CFCE2Lib.RegisterExtension

---
+++ lua/autorun/server/sv_cfc_e2_lib.lua
@@ -0,0 +1,13 @@
+CFCE2Lib = {}
+
+function CFCE2Lib.RegisterExtension( name, default, description, warning )
+    name = name:Trim():lower()
+
+    local extensions = E2Lib.GetExtensions()
+    if not table.HasValue( extensions, name ) then
+        return E2Lib.RegisterExtension( name, default, description, warning )
+    end
+
+    local status = E2Lib.GetExtensionStatus( name ),
+    assert( status, "EXTENSION_DISABLED" )
+end
