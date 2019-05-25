2da86143e935c1be37cdc37135e2010873e61f4f
= <damierny@gmail.com>
2019-05-24 19:48:46 -0500

    Fix # 2

--- lua/entities/gmod_wire_expression2/core/custom/cfc_vector.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_vector.lua
@@ -1,5 +1,5 @@
 E2Lib.RegisterExtension( "cfc_e2_lib", true )
 
 e2function vector vector:reOrder(number xLoc, number yLoc, number zLoc)
-    return wantedValues = {this[xLoc],this[yLoc],this[zLoc]}
+    return {this[xLoc],this[yLoc],this[zLoc]}
 end
