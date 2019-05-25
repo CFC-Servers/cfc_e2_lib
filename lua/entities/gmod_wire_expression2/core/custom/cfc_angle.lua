05047af81839fa39441311702c4ea8341d180778
= <damierny@gmail.com>
2019-05-24 19:58:30 -0500

    Forgot one

--- lua/entities/gmod_wire_expression2/core/custom/cfc_angle.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_angle.lua
@@ -1,20 +1,5 @@
 E2Lib.RegisterExtension( "cfc_e2_lib", true )
 
 e2function angle angle:reOrder(number pitchLoc, number yawLoc, number rollLoc)
-    local angs = {this[1],this[2],this[3]}
-    local locs = {pitchLoc,yawLoc,rollLoc}
-    local wantedAngs = angs
-
-    for looplocat = 1, 3, 1 do --loop through the inputs
-        local num = locs[looplocat] --store the current input's number
-        for pitchYawRoll = 1, 3, 1 do --loop through pitch yaw and roll
-            if (pitchYawRoll == num) then -- if the current input's number is equal to pitch (1) yaw(2) roll(3)
-                wantedAngs[pitchYawRoll] = angs[looplocat] --store the current angle part ( pitch yaw or roll) into the wantedAngs output.
-            end
-        end
-    end
-    return wantedAngs
-end
-e2function angle angle:reOrder(number pitchLoc, number yawLoc, number rollLoc)
     return {this[pitchLoc],this[yawLoc],this[rollLoc]}
 end
