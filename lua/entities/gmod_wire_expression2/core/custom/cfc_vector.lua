dd7dea3d40309f1a077347a3d16c31cfabc48eab
= <damierny@gmail.com>
2019-05-24 19:45:45 -0500

    Testing compression.

--- lua/entities/gmod_wire_expression2/core/custom/cfc_vector.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_vector.lua
@@ -1,18 +1,5 @@
 E2Lib.RegisterExtension( "cfc_e2_lib", true )
 
 e2function vector vector:reOrder(number xLoc, number yLoc, number zLoc)
-    local values = {this[1],this[2],this[3]}
-    local locs = {xLoc,yLoc,zLoc}
-    local wantedValues = values
-
-    for xYZ = 1, 3, 1 do
-        for loopVals = 1, 3, 1 do
-            local value = values[loopVals]
-            local valNum = locs[loopVals]
-            if valNum == xYZ then
-                wantedValues[xYZ] = value
-            end
-        end
-    end
-    return wantedValues
+    return wantedValues = {this[xLoc],this[yLoc],this[zLoc]}
 end
