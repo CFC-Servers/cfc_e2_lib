806ff80e245a5fb153cf793dcbb0244347fdcc06
= <damierny@gmail.com>
2019-05-25 12:43:44 -0500

    make sure things output something that it is supposed to.

--- lua/entities/gmod_wire_expression2/core/custom/cfc_array.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_array.lua
@@ -1,5 +1,7 @@
 E2Lib.RegisterExtension( "cfc_e2_lib", true )
 
+local naN = 0/0
+
 e2function array array:shuffled()
     local size = #this
     for i = size, 1, -1 do
@@ -94,84 +96,84 @@ e2function entity array:firstEntity()
     return this[1] or NULL
 end
 e2function string array:firstString()
-    return this[1] or NULL
+    return this[1] or ""
 end
 e2function number array:firstNumber()
-    return this[1] or NULL
+    return this[1] or naN
 end
 e2function vector array:firstVector()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN}
 end
 e2function vector2 array:firstVector2()
-    return this[1] or NULL
+    return this[1] or {naN,naN}
 end
 e2function vector4 array:firstVector4()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN,naN}
 end
 e2function angle array:firstAngle()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN}
 end
 e2function ranger array:firstRanger()
-    return this[1] or NULL
+    return this[1] or nil
 end
 e2function bone array:firstBone()
-    return this[1] or NULL
+    return this[1] or nil
 end
 e2function matrix2 array:firstMatrix2()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN,naN}
 end
 e2function matrix array:firstMatrix()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function matrix4 array:firstMatrix4()
-    return this[1] or NULL
+    return this[1] or {naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function wirelink array:firstWirelink()
-    return this[1] or NULL
+    return this[1] or nil
 end
 e2function complex array:firstComplex()
-    return this[1] or NULL
+    return this[1] or {naN,naN}
 end
 
 e2function entity array:lastEntity()
     return this[ #this ] or NULL
 end
 e2function string array:lastString()
-    return this[ #this ] or NULL
+    return this[ #this ] or ""
 end
 e2function number array:lastNumber()
-    return this[ #this ] or NULL
+    return this[ #this ] or naN
 end
 e2function vector array:lastVector()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN}
 end
 e2function vector2 array:lastVector2()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN}
 end
 e2function vector4 array:lastVector4()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN,naN}
 end
 e2function angle array:lastAngle()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN}
 end
 e2function ranger array:lastRanger()
-    return this[ #this ] or NULL
+    return this[ #this ] or nil
 end
 e2function bone array:lastBone()
-    return this[ #this ] or NULL
+    return this[ #this ] or nil
 end
 e2function matrix2 array:lastMatrix2()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN,naN}
 end
 e2function matrix array:lastMatrix()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function matrix4 array:lastMatrix4()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function wirelink array:lastWirelink()
-    return this[ #this ] or NULL
+    return this[ #this ] or nil
 end
 e2function complex array:lastComplex()
-    return this[ #this ] or NULL
+    return this[ #this ] or {naN,naN}
 end
--- lua/entities/gmod_wire_expression2/core/custom/cfc_table.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_table.lua
@@ -4,96 +4,96 @@ e2function entity table:firstEntity()
     return this.n[ 1 ] or NULL
 end
 e2function string table:firstString()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or ""
 end
 e2function number table:firstNumber()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or naN
 end
 e2function vector table:firstVector()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN}
 end
 e2function vector2 table:firstVector2()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN}
 end
 e2function vector4 table:firstVector4()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN,naN}
 end
 e2function angle table:firstAngle()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN}
 end
 e2function ranger table:firstRanger()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or nil
 end
 e2function bone table:firstBone()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or nil
 end
 e2function matrix2 table:firstMatrix2()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN,naN}
 end
 e2function matrix table:firstMatrix()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function matrix4 table:firstMatrix4()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function wirelink table:firstWirelink()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or nil
 end
 e2function complex table:firstComplex()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {naN,naN}
 end
 e2function table table:firstTable()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {}
 end
 e2function array table:firstArray()
-    return this.n[ 1 ] or NULL
+    return this.n[ 1 ] or {}
 end
 
 e2function entity table:lastEntity()
     return this.n[ this.size ] or NULL
 end
 e2function string table:lastString()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or ""
 end
 e2function number table:lastNumber()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or naN
 end
 e2function vector table:lastVector()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN}
 end
 e2function vector2 table:lastVector2()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN}
 end
 e2function vector4 table:lastVector4()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN,naN}
 end
 e2function angle table:lastAngle()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN}
 end
 e2function ranger table:lastRanger()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or nil
 end
 e2function bone table:lastBone()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or nil
 end
 e2function matrix2 table:lastMatrix2()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN,naN}
 end
 e2function matrix table:lastMatrix()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function matrix4 table:lastMatrix4()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN,naN}
 end
 e2function wirelink table:lastWirelink()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or nil
 end
 e2function complex table:lastComplex()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {naN,naN}
 end
 e2function table table:lastTable()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {}
 end
 e2function array table:lastArray()
-    return this.n[ this.size ] or NULL
+    return this.n[ this.size ] or {}
 end
