4de9a26a3ac021ea3bda7f6998370269de39b751
plally <11771225+plally@users.noreply.github.com>
2021-01-08 16:10:12 -0500

    remove trailing whitespaces

--- lua/entities/gmod_wire_expression2/core/custom/cfc_npc.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_npc.lua
@@ -3,7 +3,7 @@ CFCE2Lib.RegisterExtension( "cfc_e2_lib", true )
 -- Made by StrawWagen
 
 
--- Setting up local functions 
+-- Setting up local functions
 
 local function hasAccess( ply )
     if not IsValid ( ply ) then return false end
@@ -16,24 +16,24 @@ end
 local function isValidNpc( npc )
     if not IsValid( npc ) then return false end
     if not npc:IsNPC() then return false end
-    
+
     return true
 end
 
 
 -- Unrestricted get functions
 
-e2function number entity:npcGetWeaponProficiency() 
+e2function number entity:npcGetWeaponProficiency()
     if not isValidNpc( this ) then return 0 end
- 
+
     return this:GetCurrentWeaponProficiency()
 end
 
-e2function number entity:npcGetDamageMultiplier() 
+e2function number entity:npcGetDamageMultiplier()
     if not isValidNpc( this ) then return 0 end
 
     if not this.cfcE2LibNpcDamageMultiplier then return 0 end
-    
+
     return this.cfcE2LibNpcDamageMultiplier
 end
 
@@ -72,80 +72,80 @@ end
 e2function void entity:npcSetScale( number scale )
     if not isValidNpc( this ) then return end
     if not hasAccess( self.player ) then return end
-    
+
     this:SetModelScale( scale, 0 )
 end
 
 e2function void entity:npcSetScale( number scale, number time )
     if not isValidNpc( this ) then return end
     if not hasAccess( self.player ) then return end
-    
+
     this:SetModelScale( scale, time )
 end
 
 e2function void entity:npcSetWeaponProficiency( number weapProf )
     if not isValidNpc( this ) then return end
     if not hasAccess( self.player ) then return end
-    
+
     this:SetCurrentWeaponProficiency( weapProf )
 end
 
-e2function void entity:npcSetDamageMultiplier( number mul ) 
+e2function void entity:npcSetDamageMultiplier( number mul )
     if not isValidNpc( this ) then return end
     if not hasAccess( self.player ) then return end
     
     this.cfcE2LibNpcDamageMultiplier = mul
 end
 
-e2function void entity:npcSetGlobalSquad( string squadIn ) 
+e2function void entity:npcSetGlobalSquad( string squadIn )
     if not isValidNpc( this ) then return end
     if not hasAccess( self.player ) then return end
-    
+
     this:SetKeyValue( "squadname", squadIn )
 end
 
 e2function entity npcCreate( string npcClass, vector positionIn )
     if not hasAccess( self.player ) then return NULL end
     local position = Vector( positionIn[1], positionIn[2], positionIn[3] )
-    
+
     local npc = ents.Create( npcClass )
     if not isValidNpc( npc ) then return NULL end
-    
+
     npc:SetPos( position )
     npc:Spawn()
-        
+
     cleanup.Add( self.player, "npcs", npc )
-    
+
     local undoLine = "npc " .. " (" .. npc:GetClass() .. ")"
-    
+
     undo.Create( "npc" )
         undo.AddEntity( npc )
         undo.SetPlayer( self.player )
     undo.Finish( undoLine )
-    
+
     return npc
 end
 
 e2function entity npcCreate( string npcClass, vector positionIn, string modelIn )
     if not hasAccess( self.player ) then return NULL end
     local position = Vector( positionIn[1], positionIn[2], positionIn[3] )
-    
+
     local npc = ents.Create( npcClass )
     if not isValidNpc( npc ) then return NULL end
-    
+
     npc:SetKeyValue( "model", modelIn )
     npc:SetPos( position )
     npc:Spawn()
-        
+
     cleanup.Add( self.player, "npcs", npc )
-    
+
     local undoLine = "npc " .. " (" .. npc:GetClass() .. ")"
-    
+
     undo.Create( "npc" )
         undo.AddEntity( npc )
         undo.SetPlayer( self.player )
     undo.Finish( undoLine )
-    
+
     return npc
 end
 
@@ -154,11 +154,11 @@ end
 
 local function scaleNPCDamage( target, dmgInfo )
     local attacker = dmgInfo:GetAttacker()
-    
+
     if not isValidNpc( attacker ) then return end
-    
+
     local dmgmul = attacker.cfcE2LibNpcDamageMultiplier
-    
+
     if not dmgmul then return end
 
     dmgInfo:ScaleDamage( dmgmul )
