d5d9bfee0dd97d41e8b473b1bf3ad1f911e25824
Arnold Ziffel <45838657+banoargeek@users.noreply.github.com>
2022-01-24 14:33:22 -0700

    Fix repeated entries for e2helper NPC functions

--- lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_cfc_e2_lib.lua
@@ -69,8 +69,6 @@ E2Helper.Descriptions["npcGetGlobalSquad(e:)"]         = "Returns an NPC's squad
 E2Helper.Descriptions["npcGetWeaponProficiency(e:)"]   = "Returns the weapon proficiency of the NPC."
 E2Helper.Descriptions["npcGetDamageMultiplier(e:)"]    = "Returns the damage multiplier of the NPC"
 E2Helper.Descriptions["npcCouldNotReach(e:e)"]         = "Returns 1 if the NPC marked ent2 as unreachable"
-E2Helper.Descriptions["npcGetDamageMultiplier(e:)"]    = "Returns the damage multiplier of the NPC."
-E2Helper.Descriptions["npcCouldNotReach(e:e)"]         = "Returns 1 if the NPC marked ent2 as unreachable."
 E2Helper.Descriptions["npcGetCurrentSchedule(e:)"]     = "Returns the npc's current schedule. See Enums/SCHED"
 
 
