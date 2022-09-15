54ba8d02b4400a44f887d232bfb8854f2423d2f5
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2022-09-15 12:31:33 -0700

    style

--- lua/entities/gmod_wire_expression2/core/custom/cfc_simfphys.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cfc_simfphys.lua
@@ -2,12 +2,11 @@ if not istable( simfphys ) then return end
 
 e2function number entity:getSimfphysHorsepower()
     if not IsValid( this ) or not this:IsVehicle() then return 0 end
-    
-    local data = simfphys.BuildVehicleInfo( this )
-    
-    if not data then return 0 end
-    
-    local horsepower = data["horsepower"] or 0
 
+    local data = simfphys.BuildVehicleInfo( this )
+    if not data then return 0 end
+
+    local horsepower = data["horsepower"] or 0
     return horsepower
+
 end
