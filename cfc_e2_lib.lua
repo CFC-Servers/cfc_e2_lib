6b663690d0d8e9ddfb834fb56c1dafa18d615a7a
Redox <mail@redox.to>
2023-08-05 05:49:03 +0100

    Add glualint workflow (#79)
    * Add glualint
    
    * Fix workflow path
    
    * Fix style

--- cfc_e2_lib.lua
+++ cfc_e2_lib.lua
@@ -3,7 +3,7 @@ E2Lib.RegisterExtension( "cfc_e2_lib", true )
 do
     local custom_functions_dir = "cfc_functions"
 
-    local list = file.Find(custom_functions_dir .. "/*.lua", "LUA")
+    local list = file.Find( custom_functions_dir .. "/*.lua", "LUA" )
     for _, filename in pairs( list ) do
         local fullPath = custom_functions_dir .. "/" .. filename
         include( fullPath )
